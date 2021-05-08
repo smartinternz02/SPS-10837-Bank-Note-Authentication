716ef0f21af7880cb1182c9e5947208e1839fd2b
SreeLakshmi214 <83296724+SreeLakshmi214@users.noreply.github.com>
2021-05-08 14:41:18 +0530

    Update LogicalRegression.java

--- LogicalRegression.java
+++ LogicalRegression.java
@@ -7,7 +7,7 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 public class LogicalRegression { 
 	public static void main(String[] args) throws Exception {
-		DataSource source =new DataSource("C:\\Users\\sreel\\eclipse-workspace\\org.ml\\src\\main\\java\\org\\ml\\bodyfat.arff");
+		DataSource source =new DataSource("C:\\Users\\sreel\\eclipse-workspace\\org.ml\\src\\main\\java\\org\\ml\\data_banknote_authentication_testing.arff");
 		Instances dataset=source.getDataSet();
 		dataset.setClassIndex(dataset.numAttributes()-1);
 		//linear Regression
